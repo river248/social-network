d5111f8f47f42afa369be59348759784f0a53dc9
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2023-07-15 19:39:22 +0700

    refactor code follow Giang

--- dashboard/src/utils/constance.js
+++ dashboard/src/utils/constance.js
@@ -29,14 +29,6 @@ export const MenuItemDefaultLayout = [
         value: '/register',
         id: '6',
     },
-    // {
-    //     label: '',
-    //     value: '',
-    // },
-    // {
-    //     label: '',
-    //     value: '',
-    // },
 ]
 
 export const fakeDataListChat = [
@@ -70,74 +62,74 @@ export const fakeDataListChat = [
         avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/357070603_246029071507624_3199350268476552198_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=72p9icGWDF4AX89zWKi&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfBHBQ6iHkC5iysYKSYB__Xih2bah1wTti_NpeLpHvQQlQ&oe=64B14A8D',
         id: '5',
     },
-    {
-        name: 'Nhân vật 7',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
-        id: '4',
-    },
-    {
-        name: 'Nhân vật 8',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
-        id: '3',
-    },
-    {
-        name: 'Nhân vật 9',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/359718357_651272357032546_2232403090146674734_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=ic3SGQ7BCFkAX9OvzlA&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfAemPEVrqq8TcIjGNhymFqvu5ruR77iOLIYTzxCW-8xvA&oe=64B30214',
-        id: '2',
-    },
-    {
-        name: 'Nhân vật 10',
-        avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/347445983_671221348372060_2600408805073153445_n.jpg?stp=dst-jpg_p960x960&_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=2E9AV8EQE40AX-pZASJ&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfBPn74JoanGFi3qAwWtr9e6G_AfZKdS-WiLeTMkDWCcyA&oe=64B1D2CF',
-        id: '1',
-    },
-    {
-        name: 'Nhân vật 1',
-        avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
-        id: '110',
-    },
-    {
-        name: 'Nhân vật 2',
-        avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
-        id: '91',
-    },
-    {
-        name: 'Nhân vật 3',
-        avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/358421080_268382939213544_2145741453509028656_n.jpg?stp=dst-jpg_p843x403&_nc_cat=102&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=pBDaAGnTF64AX9EhMAW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfDbl40Vxs407cQtLIQ_zFScngkYVissWIskytomeXCJDg&oe=64B207EA',
-        id: '81',
-    },
-    {
-        name: 'Nhân vật 4',
-        avata: 'https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/359707636_2357252891143285_8088828061646512649_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=5DPe_D6O0LQAX8VsW7P&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfDWMvmpzFVRTG1Qaw5SR0a5ZBuUODQWVDpZsiPJydjdzQ&oe=64B33E3E',
-        id: '71',
-    },
-    {
-        name: 'Nhân vật 5',
-        avata: 'https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/359425950_289632806915765_6655673962357870812_n.jpg?stp=dst-jpg_p843x403&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=7nojpSfjemoAX9aK1rn&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBEOrdPcD1MHFXvlQi1JHabi72I-0Xy_RY7O8x_K9Lz5g&oe=64B2C4F4',
-        id: '61',
-    },
-    {
-        name: 'Nhân vật 6',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/357070603_246029071507624_3199350268476552198_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=72p9icGWDF4AX89zWKi&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfBHBQ6iHkC5iysYKSYB__Xih2bah1wTti_NpeLpHvQQlQ&oe=64B14A8D',
-        id: '51',
-    },
-    {
-        name: 'Nhân vật 7',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
-        id: '41',
-    },
-    {
-        name: 'Nhân vật 8',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
-        id: '31',
-    },
-    {
-        name: 'Nhân vật 9',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/359718357_651272357032546_2232403090146674734_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=ic3SGQ7BCFkAX9OvzlA&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfAemPEVrqq8TcIjGNhymFqvu5ruR77iOLIYTzxCW-8xvA&oe=64B30214',
-        id: '21',
-    },
-    {
-        name: 'Nhân vật 10',
-        avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/347445983_671221348372060_2600408805073153445_n.jpg?stp=dst-jpg_p960x960&_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=2E9AV8EQE40AX-pZASJ&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfBPn74JoanGFi3qAwWtr9e6G_AfZKdS-WiLeTMkDWCcyA&oe=64B1D2CF',
-        id: '11',
-    },
+    // {
+    //     name: 'Nhân vật 7',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
+    //     id: '4',
+    // },
+    // {
+    //     name: 'Nhân vật 8',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
+    //     id: '3',
+    // },
+    // {
+    //     name: 'Nhân vật 9',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/359718357_651272357032546_2232403090146674734_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=ic3SGQ7BCFkAX9OvzlA&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfAemPEVrqq8TcIjGNhymFqvu5ruR77iOLIYTzxCW-8xvA&oe=64B30214',
+    //     id: '2',
+    // },
+    // {
+    //     name: 'Nhân vật 10',
+    //     avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/347445983_671221348372060_2600408805073153445_n.jpg?stp=dst-jpg_p960x960&_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=2E9AV8EQE40AX-pZASJ&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfBPn74JoanGFi3qAwWtr9e6G_AfZKdS-WiLeTMkDWCcyA&oe=64B1D2CF',
+    //     id: '1',
+    // },
+    // {
+    //     name: 'Nhân vật 1',
+    //     avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
+    //     id: '110',
+    // },
+    // {
+    //     name: 'Nhân vật 2',
+    //     avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
+    //     id: '91',
+    // },
+    // {
+    //     name: 'Nhân vật 3',
+    //     avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/358421080_268382939213544_2145741453509028656_n.jpg?stp=dst-jpg_p843x403&_nc_cat=102&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=pBDaAGnTF64AX9EhMAW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfDbl40Vxs407cQtLIQ_zFScngkYVissWIskytomeXCJDg&oe=64B207EA',
+    //     id: '81',
+    // },
+    // {
+    //     name: 'Nhân vật 4',
+    //     avata: 'https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/359707636_2357252891143285_8088828061646512649_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=5DPe_D6O0LQAX8VsW7P&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfDWMvmpzFVRTG1Qaw5SR0a5ZBuUODQWVDpZsiPJydjdzQ&oe=64B33E3E',
+    //     id: '71',
+    // },
+    // {
+    //     name: 'Nhân vật 5',
+    //     avata: 'https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/359425950_289632806915765_6655673962357870812_n.jpg?stp=dst-jpg_p843x403&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=7nojpSfjemoAX9aK1rn&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBEOrdPcD1MHFXvlQi1JHabi72I-0Xy_RY7O8x_K9Lz5g&oe=64B2C4F4',
+    //     id: '61',
+    // },
+    // {
+    //     name: 'Nhân vật 6',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/357070603_246029071507624_3199350268476552198_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=72p9icGWDF4AX89zWKi&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfBHBQ6iHkC5iysYKSYB__Xih2bah1wTti_NpeLpHvQQlQ&oe=64B14A8D',
+    //     id: '51',
+    // },
+    // {
+    //     name: 'Nhân vật 7',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
+    //     id: '41',
+    // },
+    // {
+    //     name: 'Nhân vật 8',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/358675838_584225950541383_6796279324399499661_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WLkDfVoHPigAX-iTvZP&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfCaChK5ABG0UCQz4BoPuEtzQjebbVmaiByO17nZ35WGpw&oe=64B2D1E6',
+    //     id: '31',
+    // },
+    // {
+    //     name: 'Nhân vật 9',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/359718357_651272357032546_2232403090146674734_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=ic3SGQ7BCFkAX9OvzlA&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfAemPEVrqq8TcIjGNhymFqvu5ruR77iOLIYTzxCW-8xvA&oe=64B30214',
+    //     id: '21',
+    // },
+    // {
+    //     name: 'Nhân vật 10',
+    //     avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/347445983_671221348372060_2600408805073153445_n.jpg?stp=dst-jpg_p960x960&_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=2E9AV8EQE40AX-pZASJ&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfBPn74JoanGFi3qAwWtr9e6G_AfZKdS-WiLeTMkDWCcyA&oe=64B1D2CF',
+    //     id: '11',
+    // },
 ]
