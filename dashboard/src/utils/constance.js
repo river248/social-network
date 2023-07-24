42f9c2862f330a63fb9d2e32052ceb48cc9c135f
hieuhitek <hieutran@hitek.com.vn>
2023-07-24 22:13:35 +0700

    cmt some data

--- dashboard/src/utils/constance.js
+++ dashboard/src/utils/constance.js
@@ -4,31 +4,31 @@ export const fakeDataListChat = [
         avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
         id: '10',
     },
-    {
-        name: 'Nhân vật 2',
-        avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
-        id: '9',
-    },
-    {
-        name: 'Nhân vật 3',
-        avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/358421080_268382939213544_2145741453509028656_n.jpg?stp=dst-jpg_p843x403&_nc_cat=102&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=pBDaAGnTF64AX9EhMAW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfDbl40Vxs407cQtLIQ_zFScngkYVissWIskytomeXCJDg&oe=64B207EA',
-        id: '8',
-    },
-    {
-        name: 'Nhân vật 4',
-        avata: 'https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/359707636_2357252891143285_8088828061646512649_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=5DPe_D6O0LQAX8VsW7P&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfDWMvmpzFVRTG1Qaw5SR0a5ZBuUODQWVDpZsiPJydjdzQ&oe=64B33E3E',
-        id: '7',
-    },
-    {
-        name: 'Nhân vật 5',
-        avata: 'https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/359425950_289632806915765_6655673962357870812_n.jpg?stp=dst-jpg_p843x403&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=7nojpSfjemoAX9aK1rn&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBEOrdPcD1MHFXvlQi1JHabi72I-0Xy_RY7O8x_K9Lz5g&oe=64B2C4F4',
-        id: '6',
-    },
-    {
-        name: 'Nhân vật 6',
-        avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/357070603_246029071507624_3199350268476552198_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=72p9icGWDF4AX89zWKi&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfBHBQ6iHkC5iysYKSYB__Xih2bah1wTti_NpeLpHvQQlQ&oe=64B14A8D',
-        id: '5',
-    },
+    // {
+    //     name: 'Nhân vật 2',
+    //     avata: 'https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/330784771_2426545274167012_3990565263537923216_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=SatvT4JlqesAX_XIipj&_nc_ht=scontent-sin6-1.xx&oh=00_AfASOpgKQOSR4q_KvZsvAcCbOjeGwj00a7DVJk9ngxItXg&oe=64B322C4',
+    //     id: '9',
+    // },
+    // {
+    //     name: 'Nhân vật 3',
+    //     avata: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/358421080_268382939213544_2145741453509028656_n.jpg?stp=dst-jpg_p843x403&_nc_cat=102&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=pBDaAGnTF64AX9EhMAW&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfDbl40Vxs407cQtLIQ_zFScngkYVissWIskytomeXCJDg&oe=64B207EA',
+    //     id: '8',
+    // },
+    // {
+    //     name: 'Nhân vật 4',
+    //     avata: 'https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/359707636_2357252891143285_8088828061646512649_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=5DPe_D6O0LQAX8VsW7P&_nc_ht=scontent.fsgn5-10.fna&oh=00_AfDWMvmpzFVRTG1Qaw5SR0a5ZBuUODQWVDpZsiPJydjdzQ&oe=64B33E3E',
+    //     id: '7',
+    // },
+    // {
+    //     name: 'Nhân vật 5',
+    //     avata: 'https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/359425950_289632806915765_6655673962357870812_n.jpg?stp=dst-jpg_p843x403&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=7nojpSfjemoAX9aK1rn&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBEOrdPcD1MHFXvlQi1JHabi72I-0Xy_RY7O8x_K9Lz5g&oe=64B2C4F4',
+    //     id: '6',
+    // },
+    // {
+    //     name: 'Nhân vật 6',
+    //     avata: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/357070603_246029071507624_3199350268476552198_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=72p9icGWDF4AX89zWKi&_nc_ht=scontent.fsgn5-5.fna&oh=00_AfBHBQ6iHkC5iysYKSYB__Xih2bah1wTti_NpeLpHvQQlQ&oe=64B14A8D',
+    //     id: '5',
+    // },
 ]
 
 export const dataArray = [
@@ -112,44 +112,44 @@ export const itemData = [
         img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
         title: 'Books',
     },
-    {
-        img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-        title: 'Sink',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-        title: 'Kitchen',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-        title: 'Blinds',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-        title: 'Chairs',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-        title: 'Laptop',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-        title: 'Doors',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-        title: 'Coffee',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-        title: 'Storage',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-        title: 'Candle',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-        title: 'Coffee table',
-    },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    //     title: 'Sink',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    //     title: 'Kitchen',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    //     title: 'Blinds',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    //     title: 'Chairs',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    //     title: 'Laptop',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    //     title: 'Doors',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    //     title: 'Coffee',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    //     title: 'Storage',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    //     title: 'Candle',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    //     title: 'Coffee table',
+    // },
 ]
