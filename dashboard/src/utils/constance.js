a651967816e16458edd9f2befc1490325183ae29
hieuhitek <hieutran@hitek.com.vn>
2023-07-25 21:39:09 +0700

    cmt data

--- dashboard/src/utils/constance.js
+++ dashboard/src/utils/constance.js
@@ -39,13 +39,13 @@ export const dataArray = [
         content: 'Content 1',
         time: 1690168342041,
     },
-    {
-        id: 2,
-        title: 'Title 2',
-        name: 'Name 2',
-        content: 'Content 2',
-        time: 1690238344041,
-    },
+    // {
+    //     id: 2,
+    //     title: 'Title 2',
+    //     name: 'Name 2',
+    //     content: 'Content 2',
+    //     time: 1690238344041,
+    // },
     // {
     //     id: 3,
     //     title: 'Title 3',
@@ -108,48 +108,48 @@ export const itemData = [
         img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
         title: 'Bed',
     },
-    {
-        img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-        title: 'Books',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-        title: 'Sink',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-        title: 'Kitchen',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-        title: 'Blinds',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-        title: 'Chairs',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-        title: 'Laptop',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-        title: 'Doors',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-        title: 'Coffee',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-        title: 'Storage',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-        title: 'Candle',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-        title: 'Coffee table',
-    },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    //     title: 'Books',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    //     title: 'Sink',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    //     title: 'Kitchen',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    //     title: 'Blinds',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    //     title: 'Chairs',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    //     title: 'Laptop',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    //     title: 'Doors',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    //     title: 'Coffee',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    //     title: 'Storage',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    //     title: 'Candle',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    //     title: 'Coffee table',
+    // },
 ]
