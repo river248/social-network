c8217351b995f64056da18e9dc8e47f8fcd647bc
hieuhitek <hieutran@hitek.com.vn>
2023-08-16 00:12:35 +0700

    move infinit to home

--- dashboard/src/pages/Home/Home.js
+++ dashboard/src/pages/Home/Home.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
 
 import Complementary from '~/components/Complementary'
-import InfiniteScroll from '~/pages/InfiniteScroll'
+import { fetchNewfeeds } from '~/apis'
+import PostItem from '~/components/PostItem/PostItem'
 
 function Home() {
+    const arr = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9], [])
+    const [newFeeds, setNewFeeds] = useState([])
+    const [stateLoading, setStateLoading] = useState(false)
+    const [page, _] = useState(1)
+
+    useEffect(() => {
+        const callAPI = async () => {
+            setStateLoading(true)
+            const res = await fetchNewfeeds()
+            setNewFeeds(res ?? [])
+            setStateLoading(false)
+        }
+        callAPI()
+    }, [page])
+
     return (
         <Stack sx={{ background: 'linear-gradient(to right, #c6ffdd, #fbd786, #f7797d)' }}>
             <Grid item xs={10} sm={10} md={10} lg={10}>
-                <InfiniteScroll />
+                <Stack width="55%" margin={2}>
+                    <Stack justifyContent="center" spacing={3}>
+                        {!stateLoading
+                            ? newFeeds.map((item) => <PostItem item={item} key={item.id} />)
+                            : arr.map((item) => <PostItem loading key={item} />)}
+                    </Stack>
+                </Stack>
             </Grid>
             <Complementary />
         </Stack>
