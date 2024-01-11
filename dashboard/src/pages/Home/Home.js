22a729496a25308d12beb06793b5cc7f91b083f4
hieuhitek <hieutran@hitek.com.vn>
2024-01-11 17:20:57 +0700

    test share link

--- dashboard/src/pages/Home/Home.js
+++ dashboard/src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import Stack from '@mui/material/Stack'
 import Complementary from '~/components/Complementary'
 import { fetchNewfeeds } from '~/apis'
 import PostItem from '~/components/PostItem'
+import { Helmet } from 'react-helmet'
 
 function Home() {
     const arr = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9], [])
@@ -24,6 +25,16 @@ function Home() {
 
     return (
         <Stack sx={{ background: 'linear-gradient(to right, #c6ffdd, #fbd786, #f7797d)' }}>
+            <Helmet>
+                <meta property="og:url" content="URL của bạn" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Tiêu đề của bạn" />
+                <meta property="og:description" content="Mô tả của bạn" />
+                <meta
+                    property="og:image"
+                    content="https://scontent.fsgn2-6.fna.fbcdn.net/v/t1.6435-9/68239676_1363206673842950_8039915213148913664_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=be3454&_nc_ohc=7UvlJTlV4-sAX_-V8B7&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfDXjQFo_H8Ptu1DfYSyP583ubC8Z4kTZhKMTbIbhqaeCQ&oe=65C7154D"
+                />
+            </Helmet>
             <Grid item xs={10} sm={10} md={10} lg={10}>
                 <Stack width="55%" margin={2}>
                     <Stack justifyContent="center" spacing={3}>
